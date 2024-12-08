683c8c487650b1c5fca706311d17d7722da3bace
Digx7 <37136301+Digx7@users.noreply.github.com>
2024-12-07 21:54:14 -0800

    Still working on views

--- app/controllers/navigation_controller.rb
+++ app/controllers/navigation_controller.rb
@@ -1,10 +1,14 @@
 class NavigationController < ApplicationController
   def home
+    @projects = Post.where(blog_type_id: 1, blog_status_id: 1)
+    @articles = Post.where(blog_status_id: 1).where.not(blog_type_id: 1)
   end
 
   def projects
+    @projects = Post.where(blog_type_id: 1, blog_status_id: 1)
   end
 
   def articles
+    @articles = Post.where(blog_status_id: 1).where.not(blog_type_id: 1)
   end
 end
